fe5a3eb3851edcf2e8db634d635756b08d13f3cb
Giovana Napoli <giovana.ap.napoli@outlook.com>
2025-01-19 15:16:03 -0300

    chore: add icon lib

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideNgIconsConfig } from '@ng-icons/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideNgIconsConfig({
+      size: '24px',
+    }),
+  ],
 };
